ce40da50afab211e238d3d7f28868419e5d16576
Luiz Victor <luizvictor1231@gmail.com>
2022-01-23 21:01:47 -0400

    Página de horários adicionada e responsiva. v1.0

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let btnMenu = document.querySelector('#btn-menu')
 let menu = document.querySelector('#menu')
+
 btnMenu.addEventListener('click', () => {
   menu.classList.toggle('active')
 })
@@ -10,4 +11,34 @@ window.addEventListener('click', () => {
 
 btnMenu.addEventListener('click', event => {
   event.stopPropagation()
+})
+
+//CurrentSectionLink
+
+const sections = document.querySelectorAll('section[id]')
+
+function activeCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 3
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+    const checkStart = checkpoint >= sectionTop
+    const checkEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkStart && checkEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active-link')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
+    }
+  }
+}
+
+document.addEventListener('scroll', () => {
+  activeCurrentSection()
 })
